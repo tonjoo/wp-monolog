0322899f0922758827f697eb134922066086f660
Gama Priambada <gama.priambada@gmail.com>
2018-10-02 09:50:06 +0700

    action hook on logging

--- class-wp-monolog-wrapper.php
+++ class-wp-monolog-wrapper.php
@@ -13,6 +13,7 @@ class WP_Monolog_Wrapper {
 
 	public function addDebug( $message, array $context = array() ) {
 		if ( 100 >= $this->level ) {
+			do_action( 'wp_monolog_log_debug', $message, $context );
 			return $this->logger->addDebug( $message, $context );
 		}
 		return false;
@@ -20,6 +21,7 @@ class WP_Monolog_Wrapper {
 
 	public function addInfo( $message, array $context = array() ) {
 		if ( 200 >= $this->level ) {
+			do_action( 'wp_monolog_log_info', $message, $context );
 			return $this->logger->addInfo( $message, $context );
 		}
 		return false;
@@ -27,6 +29,7 @@ class WP_Monolog_Wrapper {
 
 	public function addNotice( $message, array $context = array() ) {
 		if ( 250 >= $this->level ) {
+			do_action( 'wp_monolog_log_notice', $message, $context );
 			return $this->logger->addNotice( $message, $context );
 		}
 		return false;
@@ -34,6 +37,7 @@ class WP_Monolog_Wrapper {
 
 	public function addWarning( $message, array $context = array() ) {
 		if ( 300 >= $this->level ) {
+			do_action( 'wp_monolog_log_warning', $message, $context );
 			return $this->logger->addWarning( $message, $context );
 		}
 		return false;
@@ -41,6 +45,7 @@ class WP_Monolog_Wrapper {
 
 	public function addError( $message, array $context = array() ) {
 		if ( 400 >= $this->level ) {
+			do_action( 'wp_monolog_log_error', $message, $context );
 			return $this->logger->addError( $message, $context );
 		}
 		return false;
@@ -48,6 +53,7 @@ class WP_Monolog_Wrapper {
 
 	public function addCritical( $message, array $context = array() ) {
 		if ( 500 >= $this->level ) {
+			do_action( 'wp_monolog_log_critical', $message, $context );
 			return $this->logger->addCritical( $message, $context );
 		}
 		return false;
@@ -55,6 +61,7 @@ class WP_Monolog_Wrapper {
 
 	public function addAlert( $message, array $context = array() ) {
 		if ( 550 >= $this->level ) {
+			do_action( 'wp_monolog_log_alert', $message, $context );
 			return $this->logger->addAlert( $message, $context );
 		}
 		return false;
@@ -62,6 +69,7 @@ class WP_Monolog_Wrapper {
 
 	public function addEmergency( $message, array $context = array() ) {
 		if ( 600 >= $this->level ) {
+			do_action( 'wp_monolog_log_emergency', $message, $context );
 			return $this->logger->addEmergency( $message, $context );
 		}
 		return false;
